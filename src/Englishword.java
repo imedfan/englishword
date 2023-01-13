ae6cbef2b815c4cc18d2248667673d49c1a6f842
Artem Palkin <whisperkokomokokoho@gmail.com>
2023-01-13 14:55:24 +0200

    create menu

--- src/Englishword.java
+++ src/Englishword.java
@@ -10,28 +10,31 @@ public class Englishword {
         String pathEnglish = "src\\dic\\english.txt";
         String pathAnother = "src\\dic\\another.txt";
 
-        System.out.println("What do you want?");
-        System.out.println("1: Add a new word");
-        System.out.println("2: Start a new remember mode game");
-        System.out.println("3: Start a new translate mode game");
-        System.out.println("4: Open setting");
+        // System.out.println("What do you want?");
+        // System.out.println("1: Add a new word");
+        // System.out.println("2: Start a new remember mode game");
+        // System.out.println("3: Start a new translate mode game");
+        // System.out.println("4: Open setting");
 
-        BufferedReader bfMainMenu = new BufferedReader(new InputStreamReader(System.in));
-        String MainMenu = bfMainMenu.readLine();
+        // BufferedReader bfMainMenu = new BufferedReader(new InputStreamReader(System.in));
+        // String MainMenu = bfMainMenu.readLine();
 
-        switch (MainMenu) {
-            case "1":
-                WriteNewWord.startWrite(pathEnglish, pathAnother);
-                break;
-            case "2":
-                ReadAndGame.startRemember(pathEnglish);
-                break;
-            case "3":
-                ReadAndGame.startTranslate(pathEnglish, pathAnother);
-                break;
-            case "4":
-                Setting.mainMenu(pathDic, pathEnglish, pathAnother);
-        }
+        // switch (MainMenu) {
+        //     case "1":
+        //         WriteNewWord.startWrite(pathEnglish, pathAnother);
+        //         break;
+        //     case "2":
+        //         ReadAndGame.startRemember(pathEnglish);
+        //         break;
+        //     case "3":
+        //         ReadAndGame.startTranslate(pathEnglish, pathAnother);
+        //         break;
+        //     case "4":
+        //         Setting.mainMenu(pathDic, pathEnglish, pathAnother);
+        //         break;
+        // }
+
+        Menu.startMenu(pathDic, pathEnglish, pathAnother);
 
     }
 

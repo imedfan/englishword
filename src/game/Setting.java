083fe10ebdbfb83699f039eeff69508f3d496ef0
Artem Palkin <whisperkokomokokoho@gmail.com>
2023-01-13 15:37:35 +0200

    dir structure add+

---
+++ src/game/Setting.java
@@ -0,0 +1,90 @@
+package game;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+
+public class Setting {
+
+    public static void mainMenu(String pathDic, String pathEnglish, String pathAnother) throws IOException {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("Setting");
+        System.out.println("--------------------------------");
+        System.out.println(" ");
+        System.out.println("What do you want?");
+        System.out.println(" ");
+        System.out.println("1: Create dictonary file");
+        System.out.println("2: Delete dictonary file");
+
+
+        String sureAnswer = "DELETE";
+        String sureAnswer2 = "delete";
+        String sureAnswer3 = "Delete";
+
+        BufferedReader bfsetting = new BufferedReader(new InputStreamReader(System.in));
+        String settingChoose = bfsetting.readLine();
+
+        switch (settingChoose) {
+            case "2":
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                System.out.println("Are you sure you want to delete?");
+                System.out.println("You should print DELETE");
+                System.out.println(" ");
+                BufferedReader bfsureDelete = new BufferedReader(new InputStreamReader(System.in));
+                String sureDelete = bfsureDelete.readLine();
+                if (sureDelete.equals(sureAnswer) || sureDelete.equals(sureAnswer2) || sureDelete.equals(sureAnswer3)) {
+                    deleteDicFile(pathDic, pathEnglish, pathAnother);
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println("You dictonary deleted successfully");
+                } else {
+                    System.out.println("You don't print DELETE. Please try again");
+                }
+                break;
+            case "1":
+                createDicFile(pathDic, pathEnglish, pathAnother);
+                break;
+        }
+    }
+
+    public static void deleteDicFile(String pathDic, String pathEnglish, String pathAnother) {
+        File pathD = new File(pathDic);
+        File fileEnglish = new File(pathEnglish);
+        File fileAnother = new File(pathAnother);
+        if (fileEnglish.exists() == true || fileAnother.exists() == true) {
+            fileEnglish.delete();
+            fileAnother.delete();
+            pathD.delete();
+        } else {
+            System.out.println("You can't delete not exist file");
+        }
+
+    }
+
+    public static void createDicFile(String pathDic, String pathEnglish, String pathAnother) throws IOException {
+
+        File pathD = new File(pathDic);
+        pathD.mkdir();
+        File fileEnglish = new File(pathEnglish);
+        File fileAnother = new File(pathAnother);
+
+        if (fileEnglish.exists() == true || fileAnother.exists() == true) {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println("Files already exists");
+            System.out.println(" ");
+        } else {
+            fileEnglish.createNewFile();
+            fileAnother.createNewFile();
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println("New dictonary files created");
+            System.out.println(" ");
+        }
+
+    }
+
+}
---
+++ src/service/LineIterator.java
@@ -0,0 +1,80 @@
+package service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LineIterator implements Iterator {
+    private final BufferedReader bufferedReader;
+    private String cachedLine;
+    private boolean finished = false;
+    private BufferedReader reader;
+
+    public LineIterator(final BufferedReader reader) throws IllegalAccessException {
+        if (reader == null) {
+            throw new IllegalArgumentException("reader must not be null");
+        }
+
+        this.reader = reader;
+
+        if (reader instanceof BufferedReader) {
+            bufferedReader = (BufferedReader) reader;
+        } else {
+            bufferedReader = new BufferedReader(reader);
+        }
+
+    }
+
+    public boolean hasNext() {
+        if (cachedLine != null) {
+            return true;
+        } else if (finished) {
+            return false;
+        } else {
+            try {
+                while (true) {
+                    String line = bufferedReader.readLine();
+                    if (line == null) {
+                        finished = true;
+                        return false;
+                    } else if (isValidLine(line)) {
+                        cachedLine = line;
+                        return true;
+                    }
+                }
+
+            } catch (IOException e) {
+                close();
+                throw new IllegalStateException(e.toString());
+            }
+        }
+    }
+
+    public boolean isValidLine(String line) {
+        return true;
+    }
+
+    public Object next() {
+        return nextLine();
+    }
+
+    public String nextLine() {
+        if (!hasNext()) {
+            throw new NoSuchElementException("No next line");
+        }
+        String currentLine = cachedLine;
+        cachedLine = null;
+        return currentLine;
+    }
+
+    public void close() {
+        finished = true;
+        cachedLine = null;
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException("Remove unsupported on LineIttertor");
+    }
+
+}
